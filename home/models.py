46dc7405a437ef9d6e90bd61ae91dfa45c3fec6a
Machoka Daisy <machokadaisy25@gmail.com>
2020-11-11 02:47:05 +0300

    add house model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
+class User(AbstractUser):
+    pass
+
+class House(models.Model):
+    image=CloudinaryField("photos", blank=True, null=True)
+    description=models.TextField()
+    price=models.CharField(max_length=300)
+    category=models.CharField(max_length=300)
+    user=models.ForeingKey(User, on_delete=models.CASCADE, related_name="houses")
+    location=models.CharField(max_length=300)
