e93ab0098eaf165e5e9a4399fd1a376a6728087c
David Okwacha <osweredavid@gmail.com>
2020-11-10 16:33:58 +0300

    configured the database

--- marven/settings.py
+++ marven/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
-    'auth',
+    'authentication',
     'rest_framework',
     'rest_framework.authtoken',
 ]
@@ -82,12 +82,25 @@ WSGI_APPLICATION = 'marven.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# development
+if config('MODE')=="prod":
+   DATABASES = {
+       'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': config('DB_NAME'),
+           'USER': config('DB_USER'),
+           'PASSWORD': config('DB_PASSWORD'),
+           'HOST': config('DB_HOST'),
+           'PORT': '',
+       }
+   }
+# production
+else:
+   DATABASES = {
+       'default': dj_database_url.config(
+           default=config('DATABASE_URL')
+       )
+   }
 
 
 # Password validation
--- marven/urls.py
+++ marven/urls.py
@@ -15,14 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
-from apiURL import views
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
 
     url('admin/', admin.site.urls),
     url(r'',include('home.urls')),
-    url(r'',include('auth.urls')),
+    url(r'',include('authentication.urls')),
     url(r'^api-token-auth/', obtain_auth_token),
 
 ]
