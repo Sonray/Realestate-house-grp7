65ffbf5ebac41467a713b342e2f86486e6571736
Machoka Daisy <machokadaisy25@gmail.com>
2020-11-11 15:55:58 +0300

    add class based views for get and post methods for house model

--- home/views.py
+++ home/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .serializers import HouseSerializer
+from .models import House
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 
 # Create your views here.
+
+class HouseList(APIView):
+
+    def get(self, request, format=None):
+        all_houses = House.objects.all()
+        serializers=HouseSerializer(all_houses, many=True)
+        return Response(serializers.data)
+
+    def post(self, request, format=None):
+        serializers = HouseSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
