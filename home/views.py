863f2a7e51af6bf6fd47e4f526707a11cd61da32
Balvine <balvy075@gmail.com>
2020-11-11 19:22:00 +0300

    modified views

--- home/views.py
+++ home/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.contrib.auth.models import User
 # Create your views here.
+
+
+
+class Review (APIView):
+
+    permission_classes = (IsAdminOrReadOnly,)
+    
+    def get(self, request, format=None):
+        all_post = Review.objects.all()
+        serializers = ReviewSerializer(all_post, many=True)
+        return Response(serializers.data)
+
+    def post(self, request, format=None):
+        serializers = ReviewSerializer(data=request.data)
+
+        if serializers.is_valid():
+
+            serializers.save()
+
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
