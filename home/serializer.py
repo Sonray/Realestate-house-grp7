c3411df5a3c920d9431b3ee668e7345ee142ac6b
Balvine <balvy075@gmail.com>
2020-11-11 13:50:10 +0300

    modified serializers.py

--- home/serializer.py
+++ home/serializer.py
@@ -1,3 +1,10 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from rest_framework.validators import UniqueValidator
+from rest_framework.validators import UniqueValidatorfrom rest_framework import serializers
+from .models import Review
+
+class RevSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ('_all_')
+
