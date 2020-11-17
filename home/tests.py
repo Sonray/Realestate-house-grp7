47a2ff51bdae64ba5243dce0002d12beccac3f07
Balvine <balvy075@gmail.com>
2020-11-17 08:46:04 +0300

    made minor changes

--- home/tests.py
+++ home/tests.py
@@ -5,10 +5,16 @@ from django.test import TestCase
 
 class  ReviewtTestClass(TestCase):
     # Set up method
+  
     def setUp(self):
-        self. review = Review(id=1,post=self.post,user=self.user)
-        self.post = Post(id=1,title='Test',content='This is a test',user = self.user)
+        self.user = User(id=1, username='barl', password='1234')
+        self.user.save()
 
-    #Testing instance
     def test_instance(self):
-        self.assertTrue(isinstance(self. review, Review))
+        self.assertTrue(isinstance(self.user, User))
+
+    def test_save_user(self):
+        self.user.save()
+
+    def test_delete_user(self):
+        self.user.delete()
