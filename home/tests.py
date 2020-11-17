676d999b925b8e3171e2ff3ee9b828c8169b6f92
PatriciaAnduru <andurupatricia@gmail.com>
2020-11-17 08:44:29 +0300

    ad tests

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
-
-# Create your tests here.
+from .models import *
+class TestInquiry(TestCase):
+    def setUp(self):
+        self.user = User
+        self.user.save()
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user, User))
+    def test_location_user(self):
+        self.user.save()
+    def test_contant_user(self):
+        self.user.delete()
