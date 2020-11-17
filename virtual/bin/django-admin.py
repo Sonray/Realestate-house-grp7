b2862466df79af711582ba88d190acc27e9c5dcc
Machoka Daisy <machokadaisy25@gmail.com>
2020-11-17 23:05:37 +0300

    remove virtual environment

--- virtual/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/daisy/Desktop/Realestate-house-grp7/Realestate-house-grp7/virtual/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
