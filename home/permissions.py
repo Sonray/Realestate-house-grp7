2b20dfdde9cf422eb4b680e798b7a73a075acfbe
Machoka Daisy <machokadaisy25@gmail.com>
2020-11-18 03:10:12 +0300

    create custom permission class that allows only owner of object to modify it

--- home/permissions.py
+++ home/permissions.py
@@ -6,3 +6,13 @@ class IsAdminOrReadOnly(BasePermission):
             return True
         else:
             return request.user.is_staff
+
+class IsOwnerOrReadOnly(BasePermission):
+    '''
+    permission class that allows only owners of an object to change , delete or edit it
+    '''
+    def has_object_permission(self, request, view, obj):
+
+        if request.method in SAFE_METHODS:
+            return True
+        return obj.owner == request.user
