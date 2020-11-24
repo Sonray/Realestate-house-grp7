6a99b1a91c6cc8bddc2dc46a46972ced80e2a732
MichaelOmondi <omondimike11@gmail.com>
2020-11-24 12:58:32 +0300

    delete virtual

--- home/views.py
+++ home/views.py
@@ -24,6 +24,15 @@ class UserProfileList(APIView):
         serializers=ProfileSerializer(all_profile, many=True)
         return Response(serializers.data)
 
+     def post(self, request, format=None):
+        serializers = ProfileSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(
+                serializers.data, status=status.HTTP_201_CREATED
+                )
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 
 # Create your views here.
@@ -101,6 +110,8 @@ class RevList(APIView):
             serializers.save()
             return Response(serializers.data, status=status.HTTP_201_CREATED)
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        
 
 
     # def delete(self, request, pk, format=None):
