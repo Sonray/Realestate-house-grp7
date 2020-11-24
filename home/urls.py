a37a7e91198eb7aa1315f8cb4586e73d3484fa71
Machoka Daisy <machokadaisy25@gmail.com>
2020-11-24 12:39:59 +0300

    make changes to urls.py

--- home/urls.py
+++ home/urls.py
@@ -8,8 +8,8 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('api/houses/', views.HouseList.as_view()),
     path('api/house/<int:pk>', views.HouseDetail.as_view()),
-    url(r'^review/(?P<user_id>\d+)', views.Review.as_view()),
-    url(r'^api/get-review/(?P<pk>[0-9]+)/', views.Review.as_view()),
+    url(r'api/house/house-id/(?P<pk>[0-9]+)/$',views.HouseDetail.as_view()),
+    path('api/reviews/', views.RevList.as_view()),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
